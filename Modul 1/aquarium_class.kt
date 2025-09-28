40d31e9ba6b8fc53eb449a959abcb55400c4f52e
Xion <144648426+Xion-Enfys@users.noreply.github.com>
2025-09-28 17:17:46 +0700

    Add files via upload

---
+++ Modul 1/aquarium_class.kt
@@ -0,0 +1,42 @@
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+
+    init {
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        println("Tank: $tank")
+    }
+
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    fun printSize() {
+        println("Width: $width cm Length: $length cm Height: $height cm")
+        println("Volume: $volume liters")
+    }
+}
+
+fun main() {
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+
+    val aquarium5 = Aquarium(numberOfFish = 29)
+    aquarium5.printSize()
+}
---
+++ Modul 1/array_list_loop.kt
@@ -0,0 +1,17 @@
+
+fun main() {
+    val school = listOf("mackerel", "trout", "halibut")
+    val myList = mutableListOf("tuna", "salmon", "shark")
+    myList.remove("shark")
+
+    println(school)
+    println(myList)
+
+    println("Print data using Looping")
+
+    var i = 0
+    for (element in school) {
+        i++
+        println("$i: $element")
+    }
+}
